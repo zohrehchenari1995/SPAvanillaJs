38184ce38184ec1db4243593904120499a0fdf71
zohrehchenari <zohrehchenari2874@gmail.com>
2025-03-01 20:44:32 +0330

    edit ui

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-// ----------------------------------------SELECTING
+// ---------------------------------------------SELECTING
 const body = document.querySelector("body");
 const contentSidebar = body.querySelector(".content__sidebar");
 const chevronToggle = body.querySelector(".chevron__toggle");
@@ -6,18 +6,19 @@ const listSearch = body.querySelector(".list__search");
 const mode = body.querySelector(".mode");
 const toggleSwitch = body.querySelector(".toggle-switch");
 const modeText =body.querySelector(".mode__text");
-// END SELECTING------------------------------------
+// END SELECTING----------------------------------------
 
 
 //----------------------------------------DARK-LIGHT MODE  
 toggleSwitch.addEventListener("click",()=>{
   body.classList.toggle("dark");
 })
-// END DARK-LIGHT MODE----------------------------------
+// END DARK-LIGHT MODE-----------------------------------
 
 
 //-------------------------------------- CHEVRON TOGGLE
   chevronToggle.addEventListener("click",()=>{
     contentSidebar.classList.toggle("close");
   })
-// END CHEVRON TOGGLE---------------------------------
+// END CHEVRON TOGGLE------------------------------------
+
