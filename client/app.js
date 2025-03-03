fbb198206c18eda8ed6b2245edf94ae12f28ae98
zohrehchenari <zohrehchenari2874@gmail.com>
2025-03-03 12:04:12 +0330

    define router function

--- client/app.js
+++ client/app.js
@@ -5,20 +5,53 @@ const chevronToggle = body.querySelector(".chevron__toggle");
 const listSearch = body.querySelector(".list__search");
 const mode = body.querySelector(".mode");
 const toggleSwitch = body.querySelector(".toggle-switch");
-const modeText =body.querySelector(".mode__text");
+const modeText = body.querySelector(".mode__text");
 // END SELECTING----------------------------------------
 
-
-//----------------------------------------DARK-LIGHT MODE  
-toggleSwitch.addEventListener("click",()=>{
+//----------------------------------------DARK-LIGHT MODE
+toggleSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
-})
+});
 // END DARK-LIGHT MODE-----------------------------------
 
-
 //-------------------------------------- CHEVRON TOGGLE
-  chevronToggle.addEventListener("click",()=>{
-    contentSidebar.classList.toggle("close");
-  })
+chevronToggle.addEventListener("click", () => {
+  contentSidebar.classList.toggle("close");
+});
 // END CHEVRON TOGGLE------------------------------------
 
+
+
+// 1.WHAT VIEW SHOW USER BASE ON ROUTE?:
+function router(params){
+  const routes =[
+    {path:"/client/",view: console.log("dashboard page")},
+    {path:"/client/products",view: console.log("products page")},
+    {path:"/client/posts",view: console.log("posts page")},
+    {path:"/client/notification",view: console.log("notif page")},
+    {path:"/client/likes",view: console.log("likes page")},
+    {path:"/client/logout",view: console.log("logout page")},
+  ];
+
+  // check routes in browser url  with path user:
+ const potentialRoutes =  routes.map((item)=>{
+    return {
+      route: item,
+      isMatch: location.pathname === item.path,
+    };
+  })
+  console.log(potentialRoutes);
+
+  // find route ismatch(true):
+  const match= potentialRoutes.find((route)=>{
+    return route.isMatch;
+   });
+   console.log(match);
+}
+
+
+
+// RUN ROUTER FUNCTION WITH TRIGGER DOMCONTENTLOADED:
+document.addEventListener("DOMContentLoaded",()=>{
+  router();
+})
